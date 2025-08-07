ba2f225acd575b69897c1a357656763fabee9872
Mikhail Stepanov <mikhail.stepanov@mobirate.com>
2025-08-07 15:50:13 +0400

    feat(BaseView): add a base class for all views

---
+++ Package/Runtime/BaseView.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using R3;
+using UnityEngine;
+
+namespace UnityReactiveMVVM
+{
+    public abstract class BaseView<TViewModel> : MonoBehaviour
+        where TViewModel : class
+    {
+        private DisposableBag _disposableBag;
+        private TViewModel _viewModel;
+
+        public void Initialize(TViewModel viewModel)
+        {
+            if (EqualityComparer<TViewModel>.Default.Equals(viewModel, _viewModel))
+                return;
+
+            _viewModel = viewModel;
+            _disposableBag.Clear();
+
+            if (viewModel == null)
+                return;
+
+            InitializeNested(viewModel);
+            if (isActiveAndEnabled)
+                BindSelf(viewModel);
+        }
+
+        private void OnEnable()
+        {
+            if (_viewModel != null)
+                BindSelf(_viewModel);
+        }
+
+        private void OnDisable() => _disposableBag.Clear();
+
+        protected virtual void InitializeNested(TViewModel viewModel) { }
+        protected virtual void BindSelf(TViewModel viewModel) { }
+
+        protected void Bind<TProperty>(Observable<TProperty> property, Action<TProperty> onNext) =>
+            property.Subscribe(onNext).AddTo(ref _disposableBag);
+    }
+}
